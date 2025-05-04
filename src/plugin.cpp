df615f9c81802213caa29a8ab825709fd51bc136
Piotr Górski <piotr@prokocim.org>
2025-05-04 11:41:55 +0200

    Changes in mqtt

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,14 +1,40 @@
 #include "plugin.h"
 #include "Version.h"
 #include <mqtt/client.h>
+#include <fstream>
+#include <sstream>
 
 namespace euroscope_mqtt
 {
     void SendTestMessage()
     {
-        const std::string address = "tcp://localhost:1883";
+        // Wartości domyślne
+        std::string address = "tcp://cma.pl:1883";
+        std::string topic = "euroscope/test";
+        std::string username, password;
+
+        // Wczytanie z pliku
+        std::ifstream creds("mqtt_credentials.txt");
+        if (!creds)
+        {
+            std::cerr << "[MQTT] Nie znaleziono pliku mqtt_credentials.txt" << std::endl;
+            return;
+        }
+
+        std::string line;
+        while (std::getline(creds, line))
+        {
+            if (line.find("username=") == 0)
+                username = line.substr(9);
+            else if (line.find("password=") == 0)
+                password = line.substr(9);
+            else if (line.find("broker=") == 0)
+                address = line.substr(7);
+            else if (line.find("topic=") == 0)
+                topic = line.substr(6);
+        }
+
         const std::string client_id = "euroscope-mqtt-test-client";
-        const std::string topic = "euroscope/test";
         const std::string payload = "Test message from euroscope-mqtt";
 
         try
@@ -16,12 +42,16 @@ namespace euroscope_mqtt
             mqtt::client client(address, client_id, mqtt::create_options(MQTTVERSION_3_1_1));
             mqtt::connect_options conn_opts;
             conn_opts.set_clean_session(true);
+            conn_opts.set_user_name(username);
+            conn_opts.set_password(password);
 
             client.connect(conn_opts);
+
             auto msg = mqtt::make_message(topic, payload);
             msg->set_qos(0);
             msg->set_retained(false);
             client.publish(msg);
+
             client.disconnect();
         }
         catch (const mqtt::exception &e)
